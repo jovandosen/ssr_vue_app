f6c565d9e96796c961021d3f34003b63a9e9ea23
Jovan Dosen <43503548+dosenj@users.noreply.github.com>
2020-06-27 12:42:22 +0200

    example five added, fresh vue instance on each request

--- server.js
+++ server.js
@@ -1,24 +1,20 @@
-const Vue = require('vue')
+const createApp = require('./src/app')
 const server = require('express')()
 const renderer = require('vue-server-renderer').createRenderer({
     template: require('fs').readFileSync('./src/index.template.html', 'utf-8')
 })
 
 server.get('*', (req, res) => {
-    const app = new Vue({
-        data: {
-            url: req.url
-        },
-        template: `<div>The visited URL is: {{ url }}</div>`
-    })
-    const context = {
+    const context = { url: req.url }
+    const app = createApp(context)
+    const info = {
         title: 'Vue Ssr Application',
         meta: `
             <meta name="author" content="Jovan Dosen">
             <meta name="viewport" content="width=device-width, initial-scale=1.0">
         `
     }
-    renderer.renderToString(app, context, (err, html) => {
+    renderer.renderToString(app, info, (err, html) => {
         if(err){
             res.status(500).end('Internal Server Error')
             return
--- src/app.js
+++ src/app.js
@@ -1,1 +1,10 @@
-console.log('app content')
+const Vue = require('vue')
+
+module.exports = function createApp (context) {
+    return new Vue({
+        data: {
+            url: context.url
+        },
+        template: `<div>The visited URL is: {{ url }}</div>`
+    })
+}
